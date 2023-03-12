115cdbeaa9e9d1b6db4bffd580cbaf4b0afa99ea
Markus Bart <markus.bart@gmx.de>
2023-03-12 15:41:13 +0100

    added stand-alone mode and wifi switch

--- Homeassistant_MQTT_Light.ino
+++ Homeassistant_MQTT_Light.ino
@@ -13,34 +13,48 @@
 /*
  * TODO:
  * - add multiple lamps capability with PCA9685 controller (using effects defined in home assistant MQTT Light)
+ * - add sensor inputs for stand-alone use with PIR sensors
+ * - add WIFI hardware switch for stand-alone use
  */
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 PCA9685 pwm0;
 
+// serial outputs for debugging
 #define NODE_DEBUG false
 
-#define ON 1
-#define OFF 0
 
-// GPIO configuration
+// GPIO config
+
+// I2C
 #define SCL_PIN 5
 #define SDA_PIN 4
-//#define WW_PIN 5
-//#define CW_PIN 4
-//#define R_PIN 14
-//#define G_PIN 12
-//#define B_PIN 13
+// digital inputs for PIR sensors or similar
+#define SENSE1_PIN 12
+#define SENSE2_PIN 14
+// WIFI switch
+#define WIFI_PIN 9
+
+// stand-alone mode (i.e. WIFI disabled) timeout in seconds for external sensors to turn off the lamp again
+#define SENSE_TIMEOUT 60
 
 // LED strip white color temperatures in Kelvin
 #define WW_TEMP 2300
 #define CW_TEMP 7000
 
+// sensor logic config (normal: act on rising edge, inverted: act on falling edge)
+#define SENSE1_INVERT false
+#define SENSE2_INVERT false
+
 // valid PWM values go up to this number
 #define PWM_RANGE 4095 // 4095 means 12 bit precision
 
+
 // global variables
+
+// wifi switch
+bool wifiEnabled = false;
 
 // module name, schema: topicPrefix namePrefix uid topicPostfix
 char namePrefix[] = "ESP-dimmer-";
@@ -82,6 +96,10 @@ volatile bool flashBool = false; // this is toggled, keeps state of  lights whil
 volatile int flashProgress = 0; // must be set to value >0 to start flashing, decrements by 1 every 100ms
 volatile int flashTimer = millis(); // variable used for timing the color flashing action
 
+// stand-alone mode related values
+volatile int senseTimeout = 0; // if this is >0, the lamp is on, if it reaches 0, the lamp turns off
+volatile int senseTimer = millis(); // variable used for timing the stand-alone mode
+
 // conversion table for mapping 8bit PWM inputs to 12bit PWM outputs for smooth, linear-looking PWM
 // linear
 const uint16_t pwmTable[] = {0,16,32,48,64,80,96,112,128,144,160,176,192,208,224,240,256,272,288,304,320,336,352,368,384,400,416,432,448,464,480,496,512,528,544,560,576,592,608,624,640,656,672,688,704,720,736,752,768,784,800,816,832,848,864,880,896,912,928,944,960,976,992,1008,1024,1040,1056,1072,1088,1104,1120,1136,1152,1168,1184,1200,1216,1232,1248,1264,1280,1296,1312,1328,1344,1360,1376,1392,1408,1424,1440,1456,1472,1488,1504,1520,1536,1552,1568,1584,1600,1616,1632,1648,1664,1680,1696,1712,1728,1744,1760,1776,1792,1808,1824,1840,1856,1872,1888,1904,1920,1936,1952,1968,1984,2000,2016,2032,2048,2064,2080,2096,2112,2128,2144,2160,2176,2192,2208,2224,2240,2256,2272,2288,2304,2320,2336,2352,2368,2384,2400,2416,2432,2448,2464,2480,2496,2512,2528,2544,2560,2576,2592,2608,2624,2640,2656,2672,2688,2704,2720,2736,2752,2768,2784,2800,2816,2832,2848,2864,2880,2896,2912,2928,2944,2960,2976,2992,3008,3024,3040,3056,3072,3088,3104,3120,3136,3152,3168,3184,3200,3216,3232,3248,3264,3280,3296,3312,3328,3344,3360,3376,3392,3408,3424,3440,3456,3472,3488,3504,3520,3536,3552,3568,3584,3600,3616,3632,3648,3664,3680,3696,3712,3728,3744,3760,3776,3792,3808,3824,3840,3856,3872,3888,3904,3920,3936,3952,3968,3984,4000,4016,4032,4048,4064,4095};
@@ -103,11 +121,21 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
+
+// this ISR is called whenever one of the sense inputs triggers
+void ICACHE_RAM_ATTR isrSense(){
+  senseTimeout = SENSE_TIMEOUT;
+}
+
+
 void setup()
 {
   // configure pins
   pinMode(SCL_PIN, OUTPUT);
   pinMode(SDA_PIN, OUTPUT);
+  pinMode(SENSE1_PIN, INPUT_PULLUP);
+  pinMode(SENSE2_PIN, INPUT_PULLUP);
+  pinMode(WIFI_PIN, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
 
   // start I2C bus
@@ -117,58 +145,67 @@ void setup()
   Serial.begin(115200);
   delay(100);
   Serial.println("Setup");
+
+  // configure input ISRs
+  attachInterrupt(digitalPinToInterrupt(SENSE1_PIN), isrSense, SENSE1_INVERT?FALLING:RISING);
+  attachInterrupt(digitalPinToInterrupt(SENSE2_PIN), isrSense, SENSE2_INVERT?FALLING:RISING);
+
+  // read WIFI switch
+  wifiEnabled = (digitalRead(WIFI_PIN)==HIGH)?true:false;
   
   // setup PWM
   Serial.println("...PWM");
   pwm0.begin();
 
+  if(wifiEnabled){
+    // init wifi and MQTT
+    Serial.println("...wifi");
+    setup_wifi();
+    Serial.println("...mqtt");
+    client.setServer(MQTT_BROKER, 1883);
+    client.setCallback(receiveMessage);
+
+    // build topics - yes I know this can be made prettier, shut up!
+    Serial.println("...topics");
+    strcpy(clientName, namePrefix);
+    strcat(clientName, macAddressUid);
   
-  // init wifi and MQTT
-  Serial.println("...wifi");
-  setup_wifi();
-  Serial.println("...mqtt");
-  client.setServer(MQTT_BROKER, 1883);
-  client.setCallback(receiveMessage);
-
-
-  // build topics - yes I know this can be made prettier, shut up!
-  Serial.println("...topics");
-  strcpy(clientName, namePrefix);
-  strcat(clientName, macAddressUid);
-
-  strcpy(configTopic, topicPrefix);
-  strcat(configTopic, clientName);
-  strcat(configTopic, topicPostfixConfig);
-
-  strcpy(stateTopic, topicPrefix);
-  strcat(stateTopic, clientName);
-  strcat(stateTopic, topicPostfixState);
+    strcpy(configTopic, topicPrefix);
+    strcat(configTopic, clientName);
+    strcat(configTopic, topicPostfixConfig);
   
-  strcpy(commandTopic, topicPrefix);
-  strcat(commandTopic, clientName);
-  strcat(commandTopic, topicPostfixCommand);
+    strcpy(stateTopic, topicPrefix);
+    strcat(stateTopic, clientName);
+    strcat(stateTopic, topicPostfixState);
+    
+    strcpy(commandTopic, topicPrefix);
+    strcat(commandTopic, clientName);
+    strcat(commandTopic, topicPostfixCommand);
 
 // 128 Bits message length:                                                                                                                        ---->|
-  strcpy(configMessage0, "{\"name\":\"esp_dimmer\",\"schema\":\"json\",\"brightness\":true,\"clrm\":true,\"min_mireds\":142,\"max_mireds\":435");
-  strcpy(configMessage1, ",\"sup_clrm\":[\"color_temp\",\"rgb\"]");
-  strcpy(configMessage2, ",\"dev\":{\"identifiers\":[\"");
-  strcat(configMessage2, clientName);
-  strcpy(configMessage3, "\"],\"name\":\"esp_dimmer\"},\"stat_t\":\"");
-  strcat(configMessage3, stateTopic);
-  strcpy(configMessage4, "\",\"cmd_t\":\"");
-  strcat(configMessage4, commandTopic);
-  strcpy(configMessage5, "\",\"uniq_id\":\"");
-  strcat(configMessage5, clientName);
-  strcat(configMessage5, "\"}");
-
-  Serial.print("clientName: ");
-  Serial.println(clientName);
-  Serial.print("configTopic: ");
-  Serial.println(configTopic);
-  Serial.print("stateTopic: ");
-  Serial.println(stateTopic);
-  Serial.print("commandTopic: ");
-  Serial.println(commandTopic);
+    strcpy(configMessage0, "{\"name\":\"esp_dimmer\",\"schema\":\"json\",\"brightness\":true,\"clrm\":true,\"min_mireds\":142,\"max_mireds\":435");
+    strcpy(configMessage1, ",\"sup_clrm\":[\"color_temp\",\"rgb\"]");
+    strcpy(configMessage2, ",\"dev\":{\"identifiers\":[\"");
+    strcat(configMessage2, clientName);
+    strcpy(configMessage3, "\"],\"name\":\"esp_dimmer\"},\"stat_t\":\"");
+    strcat(configMessage3, stateTopic);
+    strcpy(configMessage4, "\",\"cmd_t\":\"");
+    strcat(configMessage4, commandTopic);
+    strcpy(configMessage5, "\",\"uniq_id\":\"");
+    strcat(configMessage5, clientName);
+    strcat(configMessage5, "\"}");
+  
+    Serial.print("clientName: ");
+    Serial.println(clientName);
+    Serial.print("configTopic: ");
+    Serial.println(configTopic);
+    Serial.print("stateTopic: ");
+    Serial.println(stateTopic);
+    Serial.print("commandTopic: ");
+    Serial.println(commandTopic);
+  }else{
+    Serial.println("WIFI disabled by hardware switch");
+  }
 
   // turn off internal LED
   Serial.println("...initial LED values");
@@ -184,42 +221,47 @@ void setup()
   Serial.println("Initialization done.");
 }
 
+
 void loop()
 {
-  if (!client.connected()) {
-    while (!client.connected()) {
-      Serial.println("(re)connecting to broker...");
-      client.connect(clientName,MQTT_USER,MQTT_PASS);
-
-      delay(100);
+  if(wifiEnabled){
+    if (!client.connected()) {
+      while (!client.connected()) {
+        Serial.println("(re)connecting to broker...");
+        client.connect(clientName,MQTT_USER,MQTT_PASS);
+  
+        delay(100);
+      }
+      // discovery topic for autoconfiguration
+      int configMessageLength = 0;
+      configMessageLength += strlen(configMessage0);
+      configMessageLength += strlen(configMessage1);
+      configMessageLength += strlen(configMessage2);
+      configMessageLength += strlen(configMessage3);
+      configMessageLength += strlen(configMessage4);
+      configMessageLength += strlen(configMessage5);
+      client.beginPublish(configTopic, configMessageLength, true);
+      client.write((const unsigned char*)configMessage0, strlen(configMessage0));
+      client.write((const unsigned char*)configMessage1, strlen(configMessage1));
+      client.write((const unsigned char*)configMessage2, strlen(configMessage2));
+      client.write((const unsigned char*)configMessage3, strlen(configMessage3));
+      client.write((const unsigned char*)configMessage4, strlen(configMessage4));
+      client.write((const unsigned char*)configMessage5, strlen(configMessage5));
+      client.endPublish();
+      // state topic for defined initial state
+      sendCurrentState();
+      // command topic subscription
+      client.subscribe(commandTopic);
+      
+      Serial.println("All MQTT topics published/subscribed, starting normal operation.");
     }
-    // discovery topic for autoconfiguration
-    int configMessageLength = 0;
-    configMessageLength += strlen(configMessage0);
-    configMessageLength += strlen(configMessage1);
-    configMessageLength += strlen(configMessage2);
-    configMessageLength += strlen(configMessage3);
-    configMessageLength += strlen(configMessage4);
-    configMessageLength += strlen(configMessage5);
-    client.beginPublish(configTopic, configMessageLength, true);
-    client.write((const unsigned char*)configMessage0, strlen(configMessage0));
-    client.write((const unsigned char*)configMessage1, strlen(configMessage1));
-    client.write((const unsigned char*)configMessage2, strlen(configMessage2));
-    client.write((const unsigned char*)configMessage3, strlen(configMessage3));
-    client.write((const unsigned char*)configMessage4, strlen(configMessage4));
-    client.write((const unsigned char*)configMessage5, strlen(configMessage5));
-    client.endPublish();
-    // state topic for defined initial state
-    sendCurrentState();
-    // command topic subscription
-    client.subscribe(commandTopic);
-    
-    Serial.println("All MQTT topics published/subscribed, starting normal operation.");
+    client.loop();
   }
-  client.loop();
   dimLoop();
   flashLoop();
+  senseLoop();
 }
+
 
 void sendCurrentState(){
 #if(NODE_DEBUG == true)
@@ -241,8 +283,11 @@ void sendCurrentState(){
   }
   char output[128];
   serializeJson(message, output);
-  client.publish(stateTopic,output,true);
+  if(wifiEnabled){
+    client.publish(stateTopic,output,true);
+  }
 }
+
 
 void receiveMessage(char* topic, byte* payload, unsigned int length) {
 #if(NODE_DEBUG == true)
@@ -266,6 +311,7 @@ void receiveMessage(char* topic, byte* payload, unsigned int length) {
   }
 
 }
+
 
 // parse message from home assistant "MQTT Light" command topic
 void parseHA(byte* payload, unsigned int length){
@@ -336,6 +382,7 @@ void parseHA(byte* payload, unsigned int length){
   }
 }
 
+
 // Calculate RGBWC values from input values and write to PWM outputs.
 // Use conversion tables for uniform brightness dimming (avoids issues with dimming at low brightness).
 // Use un-colored part (minimum of RGB) with white, add colors (difference of the values) on top.
@@ -386,6 +433,7 @@ void updateLEDs(){
   Serial.println(pwmEndValues[4]);
 #endif
 }
+
 
 void dimLoop(){
   if(flashProgress==0){ // no dimming while flashing
@@ -442,6 +490,7 @@ void dimLoop(){
   }
 }
 
+
 void flashLoop(){
   int now = millis();
   // overflow handling, as millis() will eventually overflow
@@ -488,6 +537,35 @@ void flashLoop(){
         pwmEndValues[3] = pwmStartValues[3];
         pwmEndValues[4] = pwmStartValues[4];
       }
+    }
+  }
+}
+
+
+void senseLoop(){
+  if(senseTimeout>0){
+    int now = millis();
+    // overflow handling, as millis() will eventually overflow
+    if(now-senseTimer<0){
+      senseTimer = now;
+    }
+    if(now-senseTimer > 1000){
+      senseTimer = now;
+      
+      senseTimeout--;
+      if(senseTimeout > 0){
+        // turn on lamp
+        valueRin = 255;
+        valueGin = 255;
+        valueBin = 255;
+        valueCTin = 128;
+      }else{
+        // turn off lamp
+        valueRin = 0;
+        valueGin = 0;
+        valueBin = 0;
+      }
+      updateLEDs();
     }
   }
 }
